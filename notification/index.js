62a39880f1b2943f2fbb5e5cc4997d727df3b245
olimpius2 <49524244+olimpius2@users.noreply.github.com>
2021-03-06 15:59:17 +0100

    test no 1

--- notification/index.js
+++
@@ -1,8 +0,0 @@
-const notification_button = document.getElementById("notification_button")
-
-notification_button.addEventListener("click", function(){
-  const myNotification = new Notification('Title', {body:"body"})
-  myNotification.onclick = () => {
-    console.log('notificaiton clicked!')
-  }
-})
